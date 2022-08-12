52f6b73b2a567be175c3176490223d99d9cc5af3
给给 <1102765685@qq.com>
2022-08-12 18:10:27 +0800

    v2 init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,69 +1,38 @@
+const { gungnirTheme } = require("vuepress-theme-gungnir");
+
 module.exports = {
-    title: '给给给给',
-    description: "给给给给's blog, powered by VuePress, themed by Gungnir.",
-    theme: 'vuepress-theme-gungnir',
-    head: [
-        ["link", { rel: "icon", href: "/img/logo.png" }],
-        [
-            "meta",
-            {
-                name: "viewport",
-                content: "width=device-width,initial-scale=1,user-scalable=no"
-            }
-        ]
-    ],
-    themeConfig: {
-        base: '/GeiChan-Vuepress/',
-        repo: 'GeiChan/GeiChan-Vuepress',
-        docsBranch: 'master',
+    title: '肚财',
+    description: "肚财，一个被社会抛弃的人.",
+    head: [['link', { rel: 'icon', href: '/img/logo.png' }]],
+    theme: gungnirTheme({
+        // 左上角标题
+        navbarTitle: "肚财",
+
         docsDir: 'docs',
+
+        // 对所有博客文章页启用或禁用目录, 可选，默认：true
+        catalog: false,
+
         // 作者信息
         personalInfo: {
-            name: "艾 略", // 必须：名称，将在首页、移动端侧边栏和文章作者信息处显示
-            avatar: "/img/avatar.jpeg", // 必须：头像，将在首页和移动端侧边栏显示
-            description: '「过去」这玩意儿却总是会不断的捆绑住一个人真正想追求的平静',  // 必须：个人简介，将在首页显示
-            sns: {
-                github: "GeiChan",
-                email: "wml19970930@gmail.com"
-            }
+            name: "肚 财", // 必须：名称，将在首页、移动端侧边栏和文章作者信息处显示
+            avatar: "/img/logo.png", // 必须：头像，将在首页和移动端侧边栏显示
+            description: '他没有留下适合作遗像的照片，只有一个人形。' // 必须：个人简介，将在首页显示
         },
-        // 首页
-        homeHeaderImages: {
-            local: [
-                {
-                    "path": "/img/home-bg/bg_4.png",
-                    "mask": "rgba(40, 57, 101, .4)"
-                },
-                {
-                    "path": "/img/home-bg/bg_3.png",
-                    "mask": "rgba(40, 57, 101, .4)"
-                }
-            ]
-        },
-        pages: {
-            // 链接页配置
-            links: {
-                // 可选：链接页副标题
-                subtitle: '吼哇朋友们，这是链接页',
 
-                // 可选：链接页封面图路径和蒙版
-                bgImage: {
-                    path: '/img/home-bg/bg_3.png',
-                    mask: 'rgba(64, 118, 190, 0.5)'
-                }
+
+        // 首页
+        homeHeaderImages: [
+            {
+                "path": "/img/home-bg/background.png",
+                "mask": "rgba(40, 57, 101, .4)"
             }
-        },
-        search: true,  // 可选：是否启用搜索，默认：true
-        searchMaxSuggestions: 10,  // 可选：搜索的最大结果数，默认：10
-        searchPlaceholder: "瞧一瞧，看一看啦，绝对没有空的",  // 可选：搜索栏占位文本，默认："$ grep ..."
-        searchIcon: "ri-search-2-line",  // 可选：搜索图标
-        footer: `
-                  &copy; 艾 略 2021-2022
-                  <br>
-                  Powered by <a href="https://vuepress.vuejs.org" target="_blank">VuePress</a> &
-                  <a href="https://github.com/Renovamen/vuepress-theme-gungnir" target="_blank">Gungnir</a>
-                `,
-        sidebar: [],
+        ],
+
+        // 每页文章容量
+        blogNumPerPage: 10,
+
+
         nav: [
             {
                 text: 'Home',
@@ -76,25 +45,35 @@ module.exports = {
                 icon: "fa-paw"
             }
         ],
-        pages: {
-            // 链接页配置
-            // links: {
-            //     title: "Links",
-            //     // 可选：链接页副标题
-            //     subtitle: '吼哇朋友们，这是链接页',
-            //
-            //     // 可选：链接页封面图路径和蒙版
-            //     bgImage: {
-            //         path: '/img/home-bg/bg_3.png',
-            //         mask: 'rgba(64, 118, 190, 0.5)'
-            //     }
-            // }
+
+
+        // 导航栏配置项
+        searchText: "Search",
+        searchMaxSuggestions: 10,  // 可选：搜索的最大结果数，默认：10
+        searchPlaceholder: "瞧一瞧，看一看啦，绝对没有空的",  // 可选：搜索栏占位文本，默认："$ grep ..."
+        searchIcon: "ri-search-2-line",  // 可选：搜索图标
+
+
+        themePlugins: {
+            container: {
+                // 卡片链接中关闭域名显示
+                link: {
+                    siteDomain: false  // 可选，默认："true"
+                },
+                // markdown 增强
+                mdPlus: {
+                    all: true
+                }
+            }
         },
-        mdPlus: {
-            all: true
-        },
-        // readingTime: {
-        //     excludes: ["/about", "/links"]
-        // }
-    }
+
+
+        // 页脚
+        footer: `
+                  &copy; 艾 略 2021-2022
+                  <br>
+                  Powered by <a href="https://vuepress.vuejs.org" target="_blank">VuePress</a> &
+                  <a href="https://github.com/Renovamen/vuepress-theme-gungnir" target="_blank">Gungnir</a>
+                `
+    })
 }
--- docs/posts/2022-02-18-Apache-Iceberg-入门到放弃.md
+++ docs/posts/2022-02-18-Apache-Iceberg-入门到放弃.md
@@ -3,7 +3,7 @@ title: Apache Iceberg 入门到放弃  # 博客标题（必须）
 subtitle: Apache Iceberg 简介以及特性 (随便记录一下，能看就行) # 博客副标题（可选）
 date: 2022-02-18  # 博客日期，会显示在文章头部（可选）
 header_style: image  # 是否在博客中显示封面图：`image`（显示） / `text`（不显示）（可选，默认为 `text`）
-header_img: https://gitee.com/GeiChan/picture/raw/master/img/cover_ic.jpeg  # 博客封面图（必须，即使上一项选了 `text`，图片也需要在首页显示）
+header_img: ./images/20210404/mysql/mysql_blog_cover.png  # 博客封面图（必须，即使上一项选了 `text`，图片也需要在首页显示）
 header_mask: rgba(40, 57, 101, .4)  # 封面图遮罩（可选）
 catalog: true  # 是否启用右侧目录：false / true（可选，默认为 false）
 tags:  # 博客标签
