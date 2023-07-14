c9fb75fa28f84e9b58191432ada9d2f586db6508
给给 <1102765685@qq.com>
2023-07-14 16:16:09 +0800

    Init

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,15 +1,17 @@
 const { gungnirTheme } = require("vuepress-theme-gungnir")
+// const { PlayerPluginOptions } = require("vuepress-plugin-player")
+const { giscusPlugin } = require("vuepress-plugin-giscus")
+// @ts-ignore
 import { defineUserConfig } from 'vuepress'
 import { viteBundler } from "@vuepress/bundler-vite"
-// const { giscusPlugin } = require("vuepress-plugin-giscus")
 
 export default defineUserConfig({
-    title: '肚财',
-    description: "肚财，一个被社会抛弃的人.",
+    title: '艾 略',
+    description: "一个被社会抛弃的人.",
     head: [['link', { rel: 'icon', href: '/img/logo.png' }]],
     theme: gungnirTheme({
         // 左上角标题
-        // navbarTitle: "肚财",
+        navbarTitle: "艾 略",
         repo: "GeiChan/gei.github.io",
         docsBranch: "main",
         docsDir: 'docs',
@@ -20,7 +22,7 @@ export default defineUserConfig({
 
         // 作者信息
         personalInfo: {
-            name: "肚 财", // 必须：名称，将在首页、移动端侧边栏和文章作者信息处显示
+            name: "艾 略", // 必须：名称，将在首页、移动端侧边栏和文章作者信息处显示
             avatar: "/img/avatar.jpeg", // 必须：头像，将在首页和移动端侧边栏显示
             description: '他没有留下适合作遗像的照片，只有一个人形。', // 必须：个人简介，将在首页显示
             sns: {
@@ -71,7 +73,7 @@ export default defineUserConfig({
                 }
             },
             links: {
-                title: '友链',
+                title: 'Links',
                 // 可选：链接页副标题
                 subtitle: '只不过是个人日常使用地址而已',
 
@@ -122,49 +124,53 @@ export default defineUserConfig({
     }),
 
     plugins: [
-        // dynamicTitlePlugin({
-        //     showIcon: '', // The icon displayed when the document is in the current tab.
-        //     showText: '(/≧▽≦/)咦！又好了！', // The title displayed when the document is in the current tab.
-        //     hideIcon: '', // The icon displayed when the document is not in the current tab.
-        //     hideText: '(●—●)喔哟, 崩溃啦！', // The title displayed when the document is not in the current tab.
-        //     recoverTime: 2000, // The time to recover the title after the tab is changed.
-        // }),
-        // [
-        //     // 歌单插件
-        //     // https://github.com/blog-aurora/vuepress-theme-aurora/tree/master/Aurora-plugin/vuepress-plugin-player
-        //     'player', {
-        //         //是否禁用网易云音乐，如果你选择禁用，那么就将使用本地的歌曲，请传入链接
-        //         disabledNetEaseMusic: true,
-        //         //网易云单个歌单id
-        //         songIds: [],
-        //         //网易云歌单id 只有开启播放歌单才有效 showPlaylist: true
-        //         playlist: '',
-        //         // 是否开启播放的歌曲来源于网易云歌单
-        //         showPlaylist: false,
-        //         // 向网易云请求数据时的接口
-        //         serverUrl: 'http://localhost:8000',
-        //         //本地歌曲
-        //         localSongs: {
-        //             // 所有的歌曲封面，如果你使用本地歌曲
-        //             // 对于一些歌曲或者你需要配置的本地歌曲很多
-        //             // 你不想单独的为每一首歌曲指定封面图
-        //             // 那么你可以配置此项
-        //             // 当那首歌没有配置封面的时候
-        //             // 就会使用该项配置的图片url作为封面图
-        //             coverUrl: '',
-        //             // 本地歌曲集合
-        //             songs: [
-        //                 {
-        //                     // 放在docs/.vuepress/public/bgm 目录里下的 mp3 文件
-        //                     path: '/bgm/面会菜_林生祥_.mp3',
-        //                     // 歌曲名称
-        //                     songName: '面会菜',
-        //                     // 单独为这首歌配置封面图
-        //                     cover: '/img/bgm/面会菜.png'
-        //                 }
-        //             ]
-        //         }
+        giscusPlugin({
+            repo: "GeiChan/geichan.github.io",  // 必须，string，格式：user_name/repo_name
+            repoId: "R_kgDOH55h7Q",  // 必须，string，在 Giscus 官网上生成
+            category: "Announcements",  // 必须，string
+            categoryId: "DIC_kwDOH55h7c4CX4Yd",  // 必须，string，在 Giscus 官网上生成
+            mapping: "title",  // 可选，string，default="title"
+            reactionsEnabled: "true",  // 可选，boolean，default=true
+            theme: "preferred_color_scheme", // 可选，string，default="light"
+            lang: "auto",  // 可选，string，default="auto"（跟随网站语言，如果 Giscus 不支持你的网站的语言，则会使用 "en"）
+            lazyLoad: true,  // 可选，boolean，default=false（如果设为 true，Giscus 的加载将延迟到用户滚动到评论容器附近）
+            crossorigin: "anonymous"  // 可选，string，default="anonymous"
+        }),
+
+        // 歌单插件
+        // https://github.com/blog-aurora/vuepress-theme-aurora/tree/master/Aurora-plugin/vuepress-plugin-player
+        // playerPlugin({
+        //     // 是否开启播放的歌曲来源于网易云歌单
+        //     showPlaylist: false,
+        //     //是否禁用网易云音乐，如果你选择禁用，那么就将使用本地的歌曲，请传入链接
+        //     disabledNetEaseMusic: true,
+        //     //网易云单个歌单id
+        //     songIds: [],
+        //     //网易云歌单id 只有开启播放歌单才有效 showPlaylist: true
+        //     playlist: '',
+        //     // 向网易云请求数据时的接口
+        //     serverUrl: 'http://localhost:8000',
+        //     //本地歌曲
+        //     localSongs: {
+        //         // 所有的歌曲封面，如果你使用本地歌曲
+        //         // 对于一些歌曲或者你需要配置的本地歌曲很多
+        //         // 你不想单独的为每一首歌曲指定封面图
+        //         // 那么你可以配置此项
+        //         // 当那首歌没有配置封面的时候
+        //         // 就会使用该项配置的图片url作为封面图
+        //         coverUrl: '',
+        //         // 本地歌曲集合
+        //         songs: [
+        //             {
+        //                 // 放在docs/.vuepress/public/bgm 目录里下的 mp3 文件
+        //                 path: '/bgm/面会菜_林生祥_.mp3',
+        //                 // 歌曲名称
+        //                 songName: '面会菜',
+        //                 // 单独为这首歌配置封面图
+        //                 cover: '/img/bgm/面会菜.png'
+        //             }
+        //         ]
         //     }
-        // ]
+        // })
     ]
 })
